00e1a99f0f4a7840a8143ecf8afc3fec00581204
padhu030222 <padhujds0302@gmail.com>
2025-11-03 18:26:44 +0530

    changes done in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => res.send('🚀 Hello CI/CD world, checking CICD after checking CICD implementation!'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
 
 module.exports = app;
