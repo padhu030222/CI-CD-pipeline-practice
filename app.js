0ec501ebabcf70ec056243c5ec82b574fc830d6d
padhu030222 <padhujds0302@gmail.com>
2025-11-04 11:41:14 +0530

    changes done in test.js and app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('🚀 Hello CI/CD world, checking CICD after checking CICD implementation by November 4 2025!'));
+app.get('/', (req, res) => res.send('🚀 Hello CI/CD world, checking CICD after implementation by November 4 2025 OFF the Auto Deploy in Render!'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
